9474ae673f963a7394cb2e83cc8a551532022e8a
Ian MacDonald & paul morin <student@epicodus.com>
2015-05-11 11:53:53 -0700

    Update databases to include lists table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511182021) do
+ActiveRecord::Schema.define(version: 20150511184958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150511182021) do
     t.datetime "due_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "list_id"
   end
 
 end
