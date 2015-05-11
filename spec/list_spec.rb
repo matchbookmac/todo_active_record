cd47081fabb51af7778179b71659551b6f246bef
Ian MacDonald & paul morin <student@epicodus.com>
2015-05-11 12:10:01 -0700

    Add AR relation to tasks.

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,5 +1,10 @@
 require('spec_helper')
 
-describe() do
-
+describe(List) do
+  it("tells which tasks are in it") do
+    list = List.create({:name => "list"})
+    task1 = Task.create({:description => "task1", :list_id => list.id})
+    task2 = Task.create({:description => "task2", :list_id => list.id})
+    expect(list.tasks()).to(eq([task1, task2]))
+  end
 end
