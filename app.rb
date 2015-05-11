d06cfabdf96b8d95340282d1005db6be95fd5952
Ian MacDonald & paul morin <student@epicodus.com>
2015-05-11 11:38:26 -0700

    Add not_done method to app.

--- app.rb
+++ app.rb
@@ -7,12 +7,26 @@ require('./lib/task')
 require('pg')
 
 get('/') do
+  task = Task.create({description: "hello there", done: false})
+  task2 = Task.create({description: "hiya back", done: false})
   @tasks = Task.all()
   erb(:index)
 end
 
 post('/tasks/new') do
   @task = Task.create(description: params['description'])
-  @tasks = Task.all()  
+  @tasks = Task.all()
+  erb(:index)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params['id'].to_i())
+  erb(:task_edit)
+end
+
+patch('/tasks/:id') do
+  @task = Task.find(params['id'].to_i())
+  @task.update(description: params['description'])
+  @tasks = Task.all
   erb(:index)
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,19 @@ ENV['RACK_ENV'] = 'test'
 
 require('rspec')
 require('pg')
+require('sinatra/activerecord')
+require('task')
 require('pry')
 require('./lib/list')
 require('./lib/task')
 require('sinatra')
 require('sinatra/reloader')
 require('capybara/rspec')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all().each do |task|
+      task.destroy()
+    end
+  end
+end
