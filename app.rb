6ecf5c5a530c2aa3d09760bb42f51ebed9f5496e
Ian MacDonald & paul morin <student@epicodus.com>
2015-05-11 10:23:42 -0700

    Create task class with active_record. Get app running on sinatra.

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@ require('sinatra')
 require('sinatra/reloader')
 require('sinatra/activerecord')
 also_reload('lib/**/*.rb')
-require('./lib')
-require('./task')
+require('./lib/list')
+require('./lib/task')
 require('pg')
+
+get('/') do
+  @tasks = Task.all()
+  erb(:index)
+end
+
+post('/tasks/new') do
+  @task = Task.create(description: params['description'])
+  @tasks = Task.all()  
+  erb(:index)
+end
